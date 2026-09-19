71eeb3fd8b009ceb5c4aa39b138d7d7bee850dbc
nabilakil1974-ship-it <nabilakil1974@gmail.com>
2026-09-19 20:50:42 +0300

    Refresh third secondary branch lessons on Railway

--- app/main.py
+++ app/main.py
@@ -425,7 +425,7 @@ def root():
     html = html[:head_boundary] + direct_entry_css + theme_css + html[head_boundary:]
 
     scripts = (
-        '<script src="/static/curriculum_strict.js?v=137"></script>\n'
+        '<script src="/static/curriculum_strict.js?v=138"></script>\n'
         '<script src="/static/nabil_learning_v132.js?v=139"></script>\n'
         '<script src="/static/nabil_voice_v133.js?v=133"></script>\n'
         '<script src="/static/nabil_open_tutor_v1.js?v=16"></script>\n'
