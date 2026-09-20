ef27475e0f1cb26a859f8acea952e2df88409886
nabilakil1974-ship-it <nabilakil1974@gmail.com>
2026-09-20 17:02:18 +0300

    Deploy WhatsApp voice upload with fresh tutor JavaScript asset version

--- app/main.py
+++ app/main.py
@@ -434,7 +434,7 @@ def root():
         '<script src="/static/nabil_learning_v132.js?v=139"></script>\n'
         '<script src="/static/nabil_worksheet_v1.js?v=5"></script>\n'
         '<script src="/static/nabil_voice_v133.js?v=133"></script>\n'
-        '<script src="/static/nabil_open_tutor_v1.js?v=17"></script>\n'
+        '<script src="/static/nabil_open_tutor_v1.js?v=18"></script>\n'
         '<script src="/static/nabil_ionic_diagram_fix.js?v=1"></script>\n'
         '<script src="/static/nabil_research_v1.js?v=2"></script>\n'
         '<script src="/static/nabil_book_first_preview_v1.js?v=8"></script>\n'
