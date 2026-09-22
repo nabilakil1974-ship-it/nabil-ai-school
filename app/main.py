3d5c2e7b1a97461f6c7bc03daaa112e1d5375de5
nabilakil1974-ship-it <nabilakil1974@gmail.com>
2026-09-22 19:27:37 +0300

    Bump Drive diagnostics script version to bypass cached client code

--- app/main.py
+++ app/main.py
@@ -487,7 +487,7 @@ def _build_root_html() -> str:
         '<script src="/static/nabil_learning_v132.js?v=139"></script>\n'
         '<script src="/static/nabil_worksheet_v1.js?v=5"></script>\n'
         '<script src="/static/nabil_lesson_worksheet_card_v1.js?v=1"></script>\n'
-        '<script src="/static/nabil_drive_prepared_lesson_v1.js?v=1"></script>\n'
+        '<script src="/static/nabil_drive_prepared_lesson_v1.js?v=2"></script>\n'
         '<script src="/static/nabil_voice_v133.js?v=133"></script>\n'
         '<script src="/static/nabil_open_tutor_v1.js?v=18"></script>\n'
         '<script src="/static/nabil_ionic_diagram_fix.js?v=1"></script>\n'
